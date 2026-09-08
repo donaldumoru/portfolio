1b9ef836d9df7067b942125cfd14e59c7959a26a
donaldumoru <donaldumoru101@gmail.com>
2026-09-08 21:52:01 +0200

    rough-notation type change

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,10 +2,15 @@ import { RoughNotation } from 'react-rough-notation';
 
 export default function Title({ title }: { title: string }) {
   return (
-    <h2 className="mb-4 inline-block font-(family-name:--text-titles) text-xl font-semibold text-(--light-bg)">
-      <RoughNotation type="highlight" show={true} color="var(--accent)">
+    <RoughNotation
+      type="underline"
+      show={true}
+      color="var(--accent)"
+      strokeWidth={1.5}
+    >
+      <h2 className="mb-4 inline-block font-(family-name:--text-titles) text-xl font-semibold text-(--dark-bg) dark:text-(--light-bg)">
         {title}
-      </RoughNotation>
-    </h2>
+      </h2>
+    </RoughNotation>
   );
 }
