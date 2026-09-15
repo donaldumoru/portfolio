095f6a9ce05095a4babf02e7e3ca375a8ab8dec6
donaldumoru <donaldumoru101@gmail.com>
2026-09-16 00:24:15 +0200

    tweak rough notation delay

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -7,6 +7,7 @@ export default function Title({ title }: { title: string }) {
       show={true}
       color="var(--accent)"
       strokeWidth={1.5}
+      animationDelay={1000}
     >
       <h2 className="mb-4 inline-block font-(family-name:--text-titles) text-xl font-semibold text-(--dark-bg) dark:text-(--light-bg)">
         {title}
